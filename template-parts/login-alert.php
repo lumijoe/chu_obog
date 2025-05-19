2d38355a80c6263e262871ad27ee93403f15835f
小城 <kojolumi@gamil.com>
2025-05-19 13:56:46 +0900

    add login-alert

---
+++ template-parts/login-alert.php
@@ -0,0 +1,8 @@
+<!-- login-alert.php -->
+<script src="login.js"></script>
+<script>
+    if (sessionStorage.getItem("loggedIn") !== "true") {
+        alert("ログインしてください");
+        window.location.href = "<?php echo home_url('/'); ?>";
+    }
+</script>
