509ebcca95fb692da2676b17564aac3d7b24c265
小城 <kojolumi@gamil.com>
2025-05-28 16:55:27 +0900

    404page fix

--- 404.php
+++ 404.php
@@ -1,9 +1,12 @@
 <!-- 404.php -->
 <?php get_header(); ?>
-<div id="404-page">
-    <h2>404 Not Found（ページが見つかりませんでした）</h2>
-    <p>お探しのページは存在しないか、または移動した可能性があります。</p>
-    <a href="<?php echo home_url('/'); ?>" class="btn add-icon next 404top">トップへ戻る</a>
+<div class="wrapper-404">
+    <div id="page-404">
+        <h2>404</h2>
+        <h3>PAGE NOT FOUND</h3>
+        <p>お探しのページが見つかりませんでした<br>トップページまたはメニューより再度お探しください</p>
+    </div>
+    <a href="<?php echo home_url('/'); ?>" class="btn add-icon next btn-mb-wide btn-mt-wide">トップへ戻る</a>
 </div>
 <?php
 get_footer();
