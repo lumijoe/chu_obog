015eb1d5d61f11b9fe4a25b872e1b3a5af40e55a
小城 <kojolumi@gamil.com>
2025-05-28 15:56:17 +0900

    modi topbtn

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 <div id="404-page">
     <h2>404 Not Found（ページが見つかりませんでした）</h2>
     <p>お探しのページは存在しないか、または移動した可能性があります。</p>
-    <button class="btn btn-primay add-icon"><a href="<?php echo home_url('/'); ?>">トップへ戻る</a></button>
+    <a href="<?php echo home_url('/'); ?>" class="btn add-icon next 404top">トップへ戻る</a>
 </div>
 <?php
 get_footer();
